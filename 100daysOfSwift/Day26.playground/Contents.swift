8677b5068533e5827293625e76df3b4dd0bb7931
koonzwg <koonzwg@gmail.com>
2024-03-17 13:05:33 -0500

    Initial Commit

---
+++ 100daysOfSwift/Day26.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+/*
+ Day 26
+ - BetterRest: Introduction
+ - Entering numbers with Stepper
+ - Selecting dates and times with DatePicker
+ - Working with dates
+ - Training a model with Create ML
+ */
+
+
+// BetterRest: Introduction
+/*
+ - All iphones come with a technology called 'Core ML' built right in, which allows you to write code that makes predictions about new data based on previous data it has seen.
+ 
+ - The actual app we're building is called BetterRest, and it's designed to help coffee drinkers get a good night's sleep by asking them 3 questions
+ 
+ 1. When do they want to wake up?
+ 2. Roughly how many hours of sleep do they want?
+ 3. How many cups of coffee do they drink per day?
+ 
+ Once we have those 3 values we'll feed them into Core ML to get a result telling us when they ought to go to bed
+ */
---
+++ 100daysOfSwift/_00daysOfSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00daysOfSwiftApp.swift
+//  100daysOfSwift
+//
+//  Created by William Koonz on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct _00daysOfSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
